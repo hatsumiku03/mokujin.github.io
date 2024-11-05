1000750873fb1b9983022556517810c23b3b9e88
Joselito <josalbarr@alu.edu.gva.es>
2024-11-05 23:42:02 +0100

    updating, getting somenthing function with azucena, asuka and alisa

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Start to write the script for get random character
+const characterData = [ 
+    { imagen: 'images/AlisaBosconovitch.png', audio: 'audio/Alisa.mp3', width:'500px' }, 
+    { imagen: 'images/AsukaKazama.png', audio: 'audio/Asuka.mp3', width:'200px' }, 
+    { imagen: 'images/AzucenaMilagrosOrtizCastillos.png', audio: 'audio/Azucena.mp3', width:'200px' } 
+];
+
+function showRandomCharacterXD() { 
+    const randomCharacterPick = characterData[Math.floor(Math.random() * characterData.length)]; 
+    
+    const img = document.getElementById('img'); 
+    img.src = randomCharacterPick.imagen; 
+    img.style.width = randomCharacterPick.width;
+
+    const characterSound = document.getElementById('ss'); 
+    characterSound.src = randomCharacterPick.audio; 
+    characterSound.play(); 
+}
